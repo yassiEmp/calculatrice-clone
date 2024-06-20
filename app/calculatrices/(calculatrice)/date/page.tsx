96ccf15c0e0001ab8c939a8be2657844d18c3dc4
yassi yari <yassibello20@gmail.com>
2024-06-20 15:03:28 +0200

    [delated the layout for convertisseur]

--- app/calculatrices/(calculatrice)/date/page.tsx
+++ app/calculatrices/(calculatrice)/date/page.tsx
@@ -1,12 +1,24 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
 
 const layout = () => {
-  const [type, setType] = useState("operation");
+  const [type, setType] = useState("difference");
   const [clicked, setClicked] = useState(false);
-
-
+  const [message,setMessage]= useState("Date identique")
+  const inp1:any = useRef(null)
+  const inp2:any = useRef(null)
+  function getDurée(Date1:number,Date2:number):number{
+    const Duree = Date1 - Date2 
+    return Duree
+  }
+  function handleDateChange(date1:string,date2:string){
+    const Date1 = new Date(date1)
+    console.log("Date1: ", Date1);
+    const Date2 = new Date(date2)
+    console.log("Date2: ", Date2);
+    return 
+  }
   /// remember to fix the date it must work evenn if the month or the date return 1 digit ex = 1
   const dateString = `${new Date().getFullYear()}-0${new Date().getMonth()}-0${new Date().getDay()}`
   let c= Array(999);
@@ -41,40 +53,49 @@ const layout = () => {
       >
         <button
           onClick={() => handleClick("difference")}
-          className="w-full text-left h-7 relative flex items-center p-2 hover:bg-[#e0e0e0] rounded"
-        >{clicked?
+          className={`w-full text-left h-7 relative flex items-center p-2 hover:bg-[#e0e0e0] rounded justify-between ${!clicked&&type=="operation" ? "hidden" : ""}`}
+        >{clicked&&type!=="operation"?
           <div className="absolute h-1/2 w-1 bg-blue-900 left-0 rounded "></div>:<></>}
           Difference entre les dates
+          <BsChevronDown />
         </button>
-        <BsChevronDown />
         <button
           onClick={() => handleClick("operation")}
-          className={`w-full text-left h-7 relative flex items-center p-2 hover:bg-[#e0e0e0] rounded ${
-            !clicked ? "hidden" : ""
-          } `}
-        >{clicked?
+          className={`w-full text-left h-7 relative flex items-center p-2 hover:bg-[#e0e0e0] justify-between rounded ${
+            !clicked&&type!=="operation" ? "hidden" : ""} `}
+        >{clicked&&type=="operation"?
           <div className="absolute h-1/2 w-1 bg-blue-900 left-0 rounded "></div>:<></>}
           Ajouter ou soustraire des jours
+          <BsChevronDown />
         </button>
-        <BsChevronDown />
       </div>
       {type == "difference" ? (
         <>
           <div className="flex flex-col w-[300px] gap-4 mt-3 ">
             <p className="text-[#5c5c5c]">De</p>
             <input
+              ref={inp1}
               type="date"
               className="bg-transparent w-[150px]"
               defaultValue={dateString }
+              onChange={(e)=>handleDateChange(e.currentTarget.value,inp2.current.value)}
+              
             />
           </div>
           <div className="flex flex-col w-[300px] gap-4 mt-3 ">
             <p className="text-[#5c5c5c]">A</p>
             <input
+              ref={inp2}
               type="date"
               className="bg-transparent w-[150px]"
               defaultValue={dateString }
+              onChange={(e)=>handleDateChange(e.currentTarget.value,inp1.current.value)}
+              
             />
+          </div>
+          <div className="flex flex-col w-[300px] gap-4 mt-3 ">
+            <p className="text-[#5c5c5c]">Difference</p>
+            <p className="text-2xl">{message}</p>
           </div>
         </>
       ) : (
